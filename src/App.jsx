a624d33ebc177f37720697ebf0eb731532fc70a2
ChumboMJ <tforste@gmail.com>
2024-12-24 13:49:17 -0800

    Clean up commented code and set up Square() to show two squares with the use of a fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './tictactoe.css'
 
 export default function Square() {
-    return <button className="square">X</button>;
+    return (
+        <>
+            <button className="square">X</button>;
+            <button className="square">X</button>;
+        </>
+    );
 }
 
 
